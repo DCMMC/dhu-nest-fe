e89fbc4792d10effbd8a08dbe1e93194d2ce4649
LYF1999 <lyfmagic99@gmail.com>
2018-08-31 22:43:10 +0800

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,21 @@ import { AppRoutes } from './app.routing';
 import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 import { AuthInterceptor } from './block/AuthInterceptor';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatToolbarModule, MatMenuModule, MatButtonModule } from '@angular/material';
+import { MatToolbarModule, MatMenuModule, MatButtonModule, MatCardModule, MatListModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
+import { SideBarComponent } from './layout/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeComponent,
+    SideBarComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,6 +27,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutes,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatCardModule,
+    MatListModule,
     HttpClientModule,
     MatMenuModule,
     MatButtonModule,
